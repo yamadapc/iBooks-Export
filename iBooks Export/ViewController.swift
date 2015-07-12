930f92679f1c3684913602aa26bd560aba7acbd6
yamadapc <tacla.yamada@gmail.com>
2015-07-11 21:12:42 -0400

    Initial Commit

---
+++ iBooks Export/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iBooks Export
+//
+//  Created by adam on 7/11/15.
+//  Copyright (c) 2015 pty. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
