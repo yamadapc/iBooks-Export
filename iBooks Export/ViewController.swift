1d9ab1f947c692f4866bd9efe5bbde32bad9fb7d
yamadapc <tacla.yamada@gmail.com>
2015-07-11 22:43:51 -0400

    Add initial working implementation

--- iBooks Export/ViewController.swift
+++ iBooks Export/ViewController.swift
@@ -8,12 +8,43 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+struct Book {
+    var itemName: String?
+    var displayName: String
+    var path: String
+}
+
+class ViewController: NSViewController, NSTableViewDataSource {
+    @IBOutlet weak var tableView: NSTableView!
+
+    @IBAction func onClickExtractButton(sender: AnyObject) {
+        println("Clicked")
+        let filemanager = NSFileManager()
+        var error: NSError?
+        filemanager.createDirectoryAtPath(NSHomeDirectory() + "/Documents/iBooks-exported", withIntermediateDirectories: false, attributes: nil, error: &error)
+
+        if error != nil {
+            println(error)
+        }
+
+        for book in self.books {
+            if let targetPath = self.targetPath(book) {
+                filemanager.copyItemAtPath(book.path, toPath: NSHomeDirectory() + "/Documents/iBooks-exported/" + targetPath, error: &error)
+                if error != nil {
+                    println(error)
+                }
+            }
+
+        }
+    }
+
+    var books: [Book] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.getUserBooks()
+        self.tableView.setDataSource(self)
     }
 
     override var representedObject: AnyObject? {
@@ -22,6 +53,65 @@ class ViewController: NSViewController {
         }
     }
 
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        if tableView != self.tableView {
+            return 0
+        }
 
+        return self.books.count
+    }
+
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        if tableView != self.tableView {
+            return nil
+        }
+
+        let book = self.books[row]
+        let identifier = tableColumn?.identifier
+
+        switch(identifier!) {
+            case "itemName":
+                return book.itemName
+            case "displayName":
+                return book.displayName
+            case "path":
+                return book.path.lastPathComponent
+            default:
+                return nil
+        }
+    }
+
+    private func targetPath(book: Book) -> String? {
+        if book.displayName != "" {
+            if let itemName = book.itemName {
+                return itemName + ".epub"
+            }
+            return nil
+        }
+        return book.displayName
+    }
+    
+    private func getUserBooks() {
+        let info = self.getUserBooksPlist()
+        if let infoBooks: AnyObject = info?.objectForKey("Books") {
+            for info in infoBooks as! [AnyObject] {
+                let path = info.objectForKey("path") as! String
+
+                if path.pathExtension != "epub" {
+                    continue
+                }
+
+                let displayName = info.objectForKey("BKDisplayName") as! String
+                let itemName = info.objectForKey("itemName") as? String
+                self.books.append(Book(itemName: itemName, displayName: displayName, path: path))
+            }
+        }
+    }
+
+    private func getUserBooksPlist() -> NSDictionary? {
+        let home = NSHomeDirectory()
+        let path = home + "/Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks/Books/Books.plist"
+        return NSDictionary(contentsOfFile: path)
+    }
 }
 
